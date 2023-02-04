2b66913ebb468deb47d8cc21cd01945471653afa
김현석 <hyeonsuk@gimhyeonseog-ui-MacBookAir.local>
2023-02-04 12:42:21 +0900

    feature: project init

---
+++ src/main/java/com/hyeonsuk/spaceship/SpaceshipApplication.java
@@ -0,0 +1,13 @@
+package com.hyeonsuk.spaceship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceshipApplication.class, args);
+	}
+
+}
